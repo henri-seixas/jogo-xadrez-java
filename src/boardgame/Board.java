09ad52f8d450a4ff7beb1490fc8054795eb9e85e
Henri <henriseixassouza@gmail.com>
2025-11-17 10:11:52 -0300

    Imprimindo o tabuleiro com cor

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,6 +41,20 @@ public class Board {
         pieces[position.getRows()][position.getColumns()] = piece;
         piece.position = position;
     }
+
+    public Piece removePiece(Position position){
+        if(!positionExists(position)) {
+            throw new BoardException("Posição inexistente");
+        }
+        if (piece(position) == null) {
+            return null;
+        }
+        Piece aux = piece(position);
+        aux.position = null;
+        pieces[position.getRows()][position.getColumns()] = null;
+        return aux;
+    }
+
     private boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
